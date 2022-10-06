67320a1b24254c0bf36a407b52b0803294d363e0
Лидия Дворкина <lideya832@mail.ru>
2022-10-06 08:13:45 +0300

    Исправления внесены

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,16 @@
 public class Main {
 
     public static void main(String[] args) {
-        Book warPeace = new Book ("Война и мир", levTolstoy, 1953);
+
         Author levTolstoy = new Author ("Лев", "Толстой");
+        Book warPeace = new Book ("Война и мир", levTolstoy, 1953);
         System.out.println("warPeace.name = " + warPeace.getName() + " " + warPeace.getAuthor().getName()+ " " + warPeace.getAuthor().getSurname() + warPeace.getSince());
         System.out.println("warPeace.since = " + warPeace.getSince());
         warPeace.setSince(1960);
         System.out.println("warPeace.getSince() = " + warPeace.getSince());
 
-        Book chika = new Book("Чайка" , antonChehov, 1900 );
         Author antonChehov = new Author ("Антон", "Чехов");
+        Book chika = new Book("Чайка" , antonChehov, 1900 );
         System.out.println("chika.name = " + chika.getName() + " " + chika.getAuthor().getName()+ " " + chika.getAuthor().getSurname() + chika.getSince());
         System.out.println("chika.since = " + chika.getSince());
         chika.setSince(2000);
